a2546547ad8eb43db04efe89986b829a1852ef34
Daniil Minakov <daniilminakov123@gmail.com>
2021-02-12 21:39:35 +0300

    Complete test task

--- UlystaTest/Program.cs
+++ UlystaTest/Program.cs
@@ -9,38 +9,6 @@ namespace UlystaTest
 {
     class Program
     {
-        public static void DigitsProcessor(DigitEntity entity)
-        {
-            var line0 = entity.digit[0].Trim(' ');
-            var line1 = entity.digit[1].Trim(' ');
-
-            switch (line0)
-            {
-                case "---":
-                    if (line1 == "/")
-                    {
-                        Console.Write("3");
-                    }
-                    else if (line1 == "_|")
-                    {
-                        Console.Write("2");
-                    }
-
-                    break;
-
-                case "|":
-                    Console.Write("1");
-                    break;
-
-                case "|   |":
-                    Console.Write("4");
-                    break;
-
-                case "-----":
-                    Console.Write("5");
-                    break;
-            }
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Input full path to file with file extension: ");
@@ -69,11 +37,15 @@ namespace UlystaTest
                 int rowCount = 0;
                 string line;
 
+                //first index
                 int xIndex = 0;
+                
+                //last index
                 int yIndex = 0;
                 while ((line = reader.ReadLine()) != null)
                 {
 
+                    //read 4 rows in iteration 
                     var trimLine = line.Trim(' ');
                     if ((trimLine.Length == 0))
                     {
@@ -93,116 +65,53 @@ namespace UlystaTest
                         yIndex = 0;
                     }
 
-
-
+                    //main logic
                     while (yIndex < rowsInternal[0].Length)
                     {
+                        //increase yIndex before we find end of current digit
                         if (rowsInternal[0][yIndex] == ' ')
                         {
                             yIndex++;
                             continue;
                         }
-                        for (int j = yIndex; j < rowsInternal[0].Length; j++)
+
+                        for (var rowIndex = 0; rowIndex < digitHeight; rowIndex++) 
                         {
-
-                            if (j == rowsInternal[0].Length - 1)
+                            if (rowsInternal[rowIndex][yIndex] != ' ')
                             {
-                                yIndex = j;
-
-                                break;
-                            }
-                            if (rowsInternal[0][j] != ' ')
-                            {
-                                continue;
-                            }
-
-                            else
-                            {
-                                yIndex = j;
-
-                                break;
-                            }
-
-                        }
-                        if (rowsInternal[1][yIndex] != ' ')
-                        {
-                            for (int j = yIndex; j < rowsInternal[0].Length; j++)
-                            {
-                                if (j == rowsInternal[0].Length - 1)
+                                for (int j = yIndex; j < rowsInternal[0].Length; j++)
                                 {
-                                    yIndex = j;
+                                    if (j == rowsInternal[0].Length - 1)
+                                    {
+                                        yIndex = j;
 
-                                    break;
-                                }
-                                if (rowsInternal[1][j] != ' ')
-                                {
-                                    continue;
-                                }
+                                        break;
+                                    }
+                                    if (rowsInternal[rowIndex][j] != ' ')
+                                    {
+                                        continue;
+                                    }
 
-                                else
-                                {
-                                    yIndex = j;
-                                    break;
-                                }
+                                    else
+                                    {
+                                        yIndex = j;
+                                        break;
+                                    }
 
-                            }
-                        }
-                        if (rowsInternal[2][yIndex] != ' ')
-                        {
-                            for (int j = yIndex; j < rowsInternal[0].Length; j++)
-                            {
-                                if (j == rowsInternal[0].Length - 1)
-                                {
-                                    yIndex = j;
-
-                                    break;
                                 }
-                                if (rowsInternal[2][j] != ' ')
-                                {
-                                    continue;
-                                }
-
-                                else
-                                {
-                                    yIndex = j;
-                                    break;
-                                }
-
-                            }
-                        }
-                        if (rowsInternal[3][yIndex] != ' ')
-                        {
-                            for (int j = yIndex; j < rowsInternal[0].Length; j++)
-                            {
-                                if (j == rowsInternal[0].Length - 1)
-                                {
-                                    yIndex = j;
-
-                                    break;
-                                }
-                                if (rowsInternal[3][j] != ' ')
-                                {
-                                    continue;
-                                }
-
-                                else
-                                {
-                                    yIndex = j;
-                                    break;
-                                }
-
                             }
                         }
 
-                        DigitEntity entity = new DigitEntity();
+                        string[] digit = new string[4];
 
                         for (int a = 0; a < 4; a++)
                         {
-                            entity.digit[a] = rowsInternal[a].Substring(xIndex, yIndex + 1 - xIndex);
+                             digit[a] = rowsInternal[a].Substring(xIndex, yIndex + 1 - xIndex);
                         }
 
-                        DigitsProcessor(entity);
+                        DigitsProcessor(digit);
 
+                        //save last index in first index
                         xIndex = yIndex;
                         yIndex++;
 
@@ -210,10 +119,6 @@ namespace UlystaTest
 
                     Console.WriteLine();
                 }
-
-
-
-
             }
             catch (Exception ex)
             {
@@ -223,11 +128,38 @@ namespace UlystaTest
             {
                 reader.Dispose();
             }
+        }
+        public static void DigitsProcessor(string[] digit)
+        {
+            var line0 = digit[0].Trim(' ');
+            var line1 = digit[1].Trim(' ');
 
+            switch (line0)
+            {
+                case "---":
+                    if (line1 == "/")
+                    {
+                        Console.Write("3");
+                    }
+                    else if (line1 == "_|")
+                    {
+                        Console.Write("2");
+                    }
 
+                    break;
 
+                case "|":
+                    Console.Write("1");
+                    break;
 
+                case "|   |":
+                    Console.Write("4");
+                    break;
 
+                case "-----":
+                    Console.Write("5");
+                    break;
+            }
         }
 
     }
