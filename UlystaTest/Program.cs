9e2704dc3353126929ac7c213eb5ccdb25317bfa
Daniil Minakov <daniilminakov123@gmail.com>
2021-02-12 21:16:45 +0300

    Complete draft

--- UlystaTest/Program.cs
+++ UlystaTest/Program.cs
@@ -13,8 +13,6 @@ namespace UlystaTest
         {
             var line0 = entity.digit[0].Trim(' ');
             var line1 = entity.digit[1].Trim(' ');
-            var line2 = entity.digit[2].Trim(' ');
-            var line3 = entity.digit[3].Trim(' ');
 
             switch (line0)
             {
@@ -42,87 +40,179 @@ namespace UlystaTest
                     Console.Write("5");
                     break;
             }
-
-
-
         }
         static void Main(string[] args)
         {
-            string filePath = @"c:\Users\minak\Desktop\NumberParser.txt";
-            var sr = new StreamReader(filePath);
+            Console.WriteLine("Input full path to file with file extension: ");
+            StreamReader reader = null;
+
+            while (true)
+            {
+                string filePath = Console.ReadLine();
+
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("File not found, please input correct path :");
+                    continue;
+                }
+                else
+                {
+                    reader = new StreamReader(filePath);
+                    break;
+                }
+            }
+
             try
             {
-                int lineCounter = 0;
-                int digitHeight = 4;
+                const int digitHeight = 4;
+                string[] rowsInternal = new string[digitHeight];
+                int rowCount = 0;
+                string line;
 
-
-
-                //string[][] rows = new string[digitHeight + 1][];
-
-                string[] rowsInternal = new string[4];
-
-                //var trimLine = line.Trim(' ');
-
-                //if (trimLine.Length == 0)
-                //{
-                //    line = sr.ReadLine();
-                //    continue;
-                //}
-                int rowCounter = 0;
-                rowsInternal[0] = sr.ReadLine();
-                rowsInternal[1] = sr.ReadLine();
-                rowsInternal[2] = sr.ReadLine();
-                rowsInternal[3] = sr.ReadLine();
-
-
-                int xDelimiterIndex = 0;
-                int yDelimiterIndex = 0;
-
-
-                while (yDelimiterIndex != rowsInternal[0].Length)
+                int xIndex = 0;
+                int yIndex = 0;
+                while ((line = reader.ReadLine()) != null)
                 {
 
-
-                    if (rowsInternal[0][yDelimiterIndex] == ' ')
+                    var trimLine = line.Trim(' ');
+                    if ((trimLine.Length == 0))
                     {
-                        yDelimiterIndex++;
                         continue;
                     }
-
-                    for (int i = 0; i < rowsInternal.Length; i++)
+                    if (rowCount < digitHeight - 1)
                     {
+                        rowsInternal[rowCount] = line;
+                        rowCount++;
+                        continue;
+                    }
+                    else if (rowCount == digitHeight - 1)
+                    {
+                        rowsInternal[rowCount] = line;
+                        rowCount = 0;
+                        xIndex = 0;
+                        yIndex = 0;
+                    }
 
-                        for (int j = yDelimiterIndex; j < rowsInternal[0].Length; j++)
+
+
+                    while (yIndex < rowsInternal[0].Length)
+                    {
+                        if (rowsInternal[0][yIndex] == ' ')
                         {
+                            yIndex++;
+                            continue;
+                        }
+                        for (int j = yIndex; j < rowsInternal[0].Length; j++)
+                        {
+
+                            if (j == rowsInternal[0].Length - 1)
+                            {
+                                yIndex = j;
+
+                                break;
+                            }
                             if (rowsInternal[0][j] != ' ')
                             {
                                 continue;
                             }
+
                             else
                             {
-                                yDelimiterIndex = j;
+                                yIndex = j;
+
                                 break;
                             }
 
                         }
-                        if (rowsInternal[i][yDelimiterIndex] != ' ')
+                        if (rowsInternal[1][yIndex] != ' ')
                         {
-                            continue;
+                            for (int j = yIndex; j < rowsInternal[0].Length; j++)
+                            {
+                                if (j == rowsInternal[0].Length - 1)
+                                {
+                                    yIndex = j;
+
+                                    break;
+                                }
+                                if (rowsInternal[1][j] != ' ')
+                                {
+                                    continue;
+                                }
+
+                                else
+                                {
+                                    yIndex = j;
+                                    break;
+                                }
+
+                            }
                         }
+                        if (rowsInternal[2][yIndex] != ' ')
+                        {
+                            for (int j = yIndex; j < rowsInternal[0].Length; j++)
+                            {
+                                if (j == rowsInternal[0].Length - 1)
+                                {
+                                    yIndex = j;
+
+                                    break;
+                                }
+                                if (rowsInternal[2][j] != ' ')
+                                {
+                                    continue;
+                                }
+
+                                else
+                                {
+                                    yIndex = j;
+                                    break;
+                                }
+
+                            }
+                        }
+                        if (rowsInternal[3][yIndex] != ' ')
+                        {
+                            for (int j = yIndex; j < rowsInternal[0].Length; j++)
+                            {
+                                if (j == rowsInternal[0].Length - 1)
+                                {
+                                    yIndex = j;
+
+                                    break;
+                                }
+                                if (rowsInternal[3][j] != ' ')
+                                {
+                                    continue;
+                                }
+
+                                else
+                                {
+                                    yIndex = j;
+                                    break;
+                                }
+
+                            }
+                        }
+
+                        DigitEntity entity = new DigitEntity();
+
+                        for (int a = 0; a < 4; a++)
+                        {
+                            entity.digit[a] = rowsInternal[a].Substring(xIndex, yIndex + 1 - xIndex);
+                        }
+
+                        DigitsProcessor(entity);
+
+                        xIndex = yIndex;
+                        yIndex++;
+
                     }
 
-
-                    DigitEntity entity = new DigitEntity();
-
-                    for (int a = 0; a < 4; a++)
-                    {
-                        entity.digit[a] = rowsInternal[a].Substring(xDelimiterIndex, yDelimiterIndex - xDelimiterIndex);
-                    }
-
-                    DigitsProcessor(entity);
-                    xDelimiterIndex = yDelimiterIndex;
-
+                    Console.WriteLine();
                 }
+
+
+
 
             }
             catch (Exception ex)
@@ -131,7 +221,7 @@ namespace UlystaTest
             }
             finally
             {
-                sr.Dispose();
+                reader.Dispose();
             }
 
 
